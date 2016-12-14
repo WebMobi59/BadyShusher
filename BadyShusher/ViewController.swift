58aef75a815a1b9ff920ced6f3e3cfd732549e92
SelfnessAid <daxianjin59@hotmail.com>
2016-12-15 04:20:18 +0800

    Initial Commit

---
+++ BadyShusher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BadyShusher
+//
+//  Created by bluedragon on 12/15/16.
+//  Copyright © 2016 mobiledeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
